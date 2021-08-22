7e9cd104a9db1cc3a777c04da408a4f4f54fc4c3
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-08-21 22:41:45 -0300

    jest config

--- src/modules/users/useCases/createUser/__tests__/CreateUserUseCase.spec.ts
+++ src/modules/users/useCases/createUser/__tests__/CreateUserUseCase.spec.ts
@@ -1,5 +1,23 @@
+import { InMemoryUsersRepository } from "../../../repositories/in-memory/InMemoryUsersRepository"
 import { CreateUserUseCase } from "../CreateUserUseCase"
 
 describe('Create User Use Case', () => {
-  let createUser: CreateUserUseCase
+  let usersRepository: InMemoryUsersRepository;
+  let createUser: CreateUserUseCase;
+
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    createUser = new CreateUserUseCase(usersRepository)
+  })
+
+  it('should create a user', async () => {
+    const user = await createUser.execute(
+      {
+        name: 'name',
+        email: 'foo@bar.com',
+        password: 'password'
+      });
+
+    expect(user).toHaveProperty('id')
+  })
 })
