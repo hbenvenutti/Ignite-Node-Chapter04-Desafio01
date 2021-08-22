355d208fef19e4161b83c824c1d6bb30d6d45697
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-08-21 23:23:27 -0300

    authenticate user tests

--- src/modules/users/useCases/authenticateUser/__tests__/AuthenticateUser.spec.ts
+++ src/modules/users/useCases/authenticateUser/__tests__/AuthenticateUser.spec.ts
@@ -1,6 +1,7 @@
 import { hash } from "bcryptjs";
 import { InMemoryUsersRepository } from "../../../repositories/in-memory/InMemoryUsersRepository";
 import { AuthenticateUserUseCase } from "../AuthenticateUserUseCase";
+import { IncorrectEmailOrPasswordError } from "../IncorrectEmailOrPasswordError";
 
 describe('Authenticate User', () => {
   let usersRepository: InMemoryUsersRepository;
@@ -19,5 +20,24 @@ describe('Authenticate User', () => {
     const response = await authenticateUser.execute({email, password: 'password'})
 
     expect(response).toHaveProperty('user');
+    expect(response).toHaveProperty('token');
+  });
+
+  it('should not authenticate an inexistent user', async () => {
+    const email = 'foo@bar.com';
+
+    expect(async () => {
+      await authenticateUser.execute({email, password: 'password'})
+    }).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError)
+  })
+
+  it('should not authenticate user with incorrect password ', async () => {
+    const email = 'foo@bar.com';
+    const password = await hash('password', 8);
+    await usersRepository.create({ name: 'foo', email, password})
+
+    expect(async () => {
+      await authenticateUser.execute({email, password: 'wrong-password'})
+    }).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError)
   })
 });
