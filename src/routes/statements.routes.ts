9813fa5a303d079aca98b90f18fc365a43aa18fd
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-09-05 17:43:02 -0300

    Transaction type removed

--- src/routes/statements.routes.ts
+++ src/routes/statements.routes.ts
@@ -16,5 +16,6 @@ statementRouter.get('/balance', getBalanceController.execute);
 statementRouter.post('/deposit', createStatementController.execute);
 statementRouter.post('/withdraw', createStatementController.execute);
 statementRouter.get('/:statement_id', getStatementOperationController.execute);
+statementRouter.post('/transfers/:recipient_id')
 
 export { statementRouter };
